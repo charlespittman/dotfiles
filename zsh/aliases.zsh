5b7437391d18e710462e329be2b26139012bd08e
Charles Pittman <charles.pittman@gmail.com>
2015-01-27 11:09:10 -0500

    shortening common commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,14 @@ alias reload!='. ~/.zshrc'
 
 alias pd='pushd'
 alias po='popd'
+
 alias trash='gvfs-trash'
+
+alias l='ls -lah'           # Long view, show hidden
+alias la='ls -AF'           # Compact view, show hidden
+alias ll='ls -lFh'          # Long view, no hidden
+
+alias ping='ping -c 5'      # Pings with 5 packets, not unlimited
+
+alias df='df -h'            # Disk free, in gigabytes, not bytes
+alias du='du -h -c'         # Calculate total disk usage for a folder
