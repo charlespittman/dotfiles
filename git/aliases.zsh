227cd638c4150183b45132bcaec40b921a8510e0
Charles Pittman <charles.pittman@gmail.com>
2016-05-16 10:50:15 -0400

    Updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,6 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gsd='git svn dcommit'
+alias gsf='git svn fetch'
+alias gsr='git svn rebase'
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,6 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+#Sets default format for 'ri'
+export RI="--format ansi"
